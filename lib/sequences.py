8011d9089f3c37494dec19a451d9ab928aeb1820
Abdullahi-Ai <abdullahiibrashair@gmail.com>
2025-05-08 17:41:19 +0300

    python-p3-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length < 0:
+        print("Please enter a positive integer")
+        return
+
+    fibonacci_sequence = []
+
+    if length == 0:
+        print(fibonacci_sequence)
+        return
+
+    a, b = 0, 1
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+
+    print(fibonacci_sequence)
+
+# example usage
+print_fibonacci(10)
